cc129c4e839eaa751e19f72ccfb9cba9ab46a9b8
DanilRellax <77833594+DanilRellax@users.noreply.github.com>
2021-02-01 08:07:34 +0200

    Add files via upload

---
+++ precache-manifest.ec610d69b8da57e8390796855784e888.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d914f68334980673de6b1301cc13ad0f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6fcc8fc9768296f31452",
+    "url": "./static/css/2.d0a510e7.chunk.css"
+  },
+  {
+    "revision": "7ecfa1f8a38df60263b5",
+    "url": "./static/css/main.bb9ae9b2.chunk.css"
+  },
+  {
+    "revision": "6fcc8fc9768296f31452",
+    "url": "./static/js/2.4a6b62bb.chunk.js"
+  },
+  {
+    "revision": "c04130d8b6692f26b622ca7bd7d5f6b5",
+    "url": "./static/js/2.4a6b62bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ecfa1f8a38df60263b5",
+    "url": "./static/js/main.aedfcc8b.chunk.js"
+  },
+  {
+    "revision": "b562a17de94e28e650bd",
+    "url": "./static/js/runtime-main.af2ca080.js"
+  },
+  {
+    "revision": "fefabe14fca7be4b18b35cb923cc6db5",
+    "url": "./static/media/PornCoin16pxNew.fefabe14.svg"
+  },
+  {
+    "revision": "336d6e3698e5dc0b6b09e39cfa957cbf",
+    "url": "./static/media/PornCoin45pxNew.336d6e36.svg"
+  },
+  {
+    "revision": "83aaa5ddd297db41bc42080e171c2b57",
+    "url": "./static/media/adventure.83aaa5dd.png"
+  },
+  {
+    "revision": "91d7f1384e5b64bfe7c530c327f0304e",
+    "url": "./static/media/billie.91d7f138.png"
+  },
+  {
+    "revision": "2f4699a69baa22b5faa4543b48e9a008",
+    "url": "./static/media/dataCenter.2f4699a6.svg"
+  },
+  {
+    "revision": "21e4ebc2295361de0e5f61eb29e90e8c",
+    "url": "./static/media/durka.21e4ebc2.png"
+  },
+  {
+    "revision": "969059b52b03f5a93d0cac73deb7da34",
+    "url": "./static/media/films.969059b5.png"
+  },
+  {
+    "revision": "c1c88d4e05cbef94bf5c85e0368e034c",
+    "url": "./static/media/graphicsСard.c1c88d4e.svg"
+  },
+  {
+    "revision": "ddba62c6e0ccbdbdfd18c28e9cd3f720",
+    "url": "./static/media/graphicsСardTwo.ddba62c6.svg"
+  },
+  {
+    "revision": "26caf96912566160fcafc5814d981843",
+    "url": "./static/media/joker.26caf969.png"
+  },
+  {
+    "revision": "ac805c08bdd3de208c825942b3f725ef",
+    "url": "./static/media/klunka.ac805c08.png"
+  },
+  {
+    "revision": "4f53c0977299a616c0c9ab0d9aaceab7",
+    "url": "./static/media/malibu.4f53c097.png"
+  },
+  {
+    "revision": "beb716f3745f27686b47f512499d2216",
+    "url": "./static/media/naruto.beb716f3.png"
+  },
+  {
+    "revision": "1a13d5aaa1bf69338e77ab7bc8604e5d",
+    "url": "./static/media/ono.1a13d5aa.png"
+  },
+  {
+    "revision": "8b59c1948ec4f3fb6e3284e82719e026",
+    "url": "./static/media/popugai.8b59c194.png"
+  },
+  {
+    "revision": "f4b170d0bfe52b1017b7ecc9d63f461b",
+    "url": "./static/media/quantumPC.f4b170d0.svg"
+  },
+  {
+    "revision": "aeacee8cab5c3595a04152146840e009",
+    "url": "./static/media/serverDC.aeacee8c.svg"
+  },
+  {
+    "revision": "625a1d19ff731c8b69ac644ee410e76a",
+    "url": "./static/media/simpsons.625a1d19.png"
+  },
+  {
+    "revision": "ff815d40f8085063375d04054f29c257",
+    "url": "./static/media/superPC.ff815d40.svg"
+  }
+]);
